a5ceaf0a2e2964824e2a7d66bf6eb79bf1d70e76
Seokhun Yoon <seokhunyoon@SOKURI-Macbook-Pro.local>
2023-05-13 01:05:26 +0900

    Add prettier

--- Client/src/apis/requests.ts
+++ Client/src/apis/requests.ts
@@ -1,6 +1,6 @@
 import { exampleInstance } from "./axiosInstances";
 
-const getExampleData = (name: string, page = 1, rpp = 10) =>
+export const getExampleData = (name: string, page = 1, rpp = 10) =>
     exampleInstance({
         url: "/get/example/data",
         method: "get",
--- Client/src/hooks/useInputFocusOnKeyDown.ts
+++ Client/src/hooks/useInputFocusOnKeyDown.ts
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const useInputFocusOnKeyDown = (inputRef: React.RefObject<HTMLInputElement>, key: string) => {
-	function focusElementOnKeyDown(e: KeyboardEvent, El: React.RefObject<HTMLInputElement>) {
-		if (!El.current) return;
-		if (e.key === key && El.current !== document.activeElement) {
-			e.preventDefault();
-			El.current.focus();
-		}
-	}
+    function focusElementOnKeyDown(e: KeyboardEvent, El: React.RefObject<HTMLInputElement>) {
+        if (!El.current) return;
+        if (e.key === key && El.current !== document.activeElement) {
+            e.preventDefault();
+            El.current.focus();
+        }
+    }
 
-	React.useEffect(() => {
-		addEventListener("keydown", (e: KeyboardEvent) => {
-			focusElementOnKeyDown(e, inputRef)
-		})
+    React.useEffect(() => {
+        addEventListener("keydown", (e: KeyboardEvent) => {
+            focusElementOnKeyDown(e, inputRef);
+        });
 
-		return () => {
-			removeEventListener("keydown", (e: KeyboardEvent) => {
-				focusElementOnKeyDown(e, inputRef)
-			})
-		}
-	}, [])
-}
+        return () => {
+            removeEventListener("keydown", (e: KeyboardEvent) => {
+                focusElementOnKeyDown(e, inputRef);
+            });
+        };
+    }, []);
+};
 
-export default useInputFocusOnKeyDown
+export default useInputFocusOnKeyDown;
