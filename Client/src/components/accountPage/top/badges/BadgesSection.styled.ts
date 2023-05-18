ac4717d93a59c5b03c4048de630a2290882d5e08
Junhyuk Park <junhyeg08@gmail.com>
2023-05-18 19:14:18 +0900

    style: account badge page

---
+++ Client/src/components/accountPage/top/badges/BadgesSection.styled.ts
@@ -0,0 +1,226 @@
+import { styled } from "styled-components";
+
+export const Frame58 = styled.div`
+    width: 1614px;
+    height: 435px;
+    position: absolute;
+    left: 155px;
+    top: 311px;
+`;
+
+export const BadgeUnknown = styled.div`
+    width: 133px;
+    height: 161px;
+    position: absolute;
+    left: 460px;
+    top: 57px;
+`;
+
+export const Group58 = styled.div`
+    position: absolute;
+    // box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.25);
+    left: 9px;
+    top: 6px;
+    right: 8px;
+    bottom: 39px;
+    position: absolute;
+    left: 6.6%;
+    right: 6.18%;
+    top: 3.73%;
+    bottom: 24.22%;
+
+    filter: drop-shadow(0px 1px 2px rgba(0, 0, 0, 0.25));
+`;
+
+export const Ellipse21 = styled.img`
+    object-fit: cover;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    height: 116px;
+    width: 116px;
+    left: 8.78125px;
+    top: 6px;
+    border-radius: 0px;
+`;
+
+export const BadgeText = styled.span`
+    text-overflow: ellipsis;
+    font-size: 14px;
+    font-family: Poppins, sans-serif;
+    font-weight: 700;
+    text-align: center;
+    position: absolute;
+    left: 34px;
+    top: 139px;
+    width: 87px;
+    height: 21px;
+    line-height: 21px;
+    color: var(--neutre-Grey-300);
+`;
+
+export const BadgeUnknown_0001 = styled.div`
+    width: 133px;
+    height: 161px;
+    position: absolute;
+    left: 649px;
+    top: 57px;
+`;
+
+export const Ellipse21_0001 = styled.img`
+    background-color: rgb(216, 213, 218);
+    border-radius: 116px / 116px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+`;
+
+export const BadgeUnknown_0002 = styled.div`
+    width: 133px;
+    height: 161px;
+    position: absolute;
+    left: 838px;
+    top: 57px;
+`;
+
+export const BadgeUnknown_0003 = styled.div`
+    width: 133px;
+    height: 161px;
+    position: absolute;
+    left: 1027px;
+    top: 57px;
+`;
+
+export const BadgeUnknown_0004 = styled.div`
+    width: 133px;
+    height: 161px;
+    position: absolute;
+    left: 1216px;
+    top: 57px;
+`;
+
+export const BadgeUnknown_0005 = styled.div`
+    width: 133px;
+    height: 161px;
+    position: absolute;
+    left: 1405px;
+    top: 57px;
+`;
+
+export const BadgeUnknown_0006 = styled.div`
+    width: 133px;
+    height: 161px;
+    position: absolute;
+    left: 460px;
+    top: 234px;
+`;
+
+export const BadgeUnknown_0007 = styled.div`
+    width: 133px;
+    height: 161px;
+    position: absolute;
+    left: 649px;
+    top: 234px;
+`;
+
+export const BadgeUnknown_0008 = styled.div`
+    width: 133px;
+    height: 161px;
+    position: absolute;
+    left: 838px;
+    top: 234px;
+`;
+
+export const BadgeUnknown_0009 = styled.div`
+    width: 133px;
+    height: 161px;
+    position: absolute;
+    left: 1027px;
+    top: 234px;
+`;
+
+export const BadgeUnknown_0010 = styled.div`
+    width: 133px;
+    height: 161px;
+    position: absolute;
+    left: 1216px;
+    top: 234px;
+`;
+
+export const BadgeUnknown_0011 = styled.div`
+    width: 133px;
+    height: 161px;
+    position: absolute;
+    left: 1405px;
+    top: 234px;
+`;
+
+export const Group78 = styled.div`
+    width: 332px;
+    height: 360px;
+    position: absolute;
+    left: 55px;
+    top: 40px;
+`;
+
+export const Rectangle84 = styled.div`
+    width: 332px;
+    height: 360px;
+    background-color: white;
+    border-radius: 32px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const Group56 = styled.div`
+    width: 240px;
+    height: 240px;
+    position: absolute;
+    left: 101px;
+    top: 59px;
+`;
+
+export const Ellipse20 = styled.img`
+    width: 240px;
+    height: 240px;
+    object-fit: cover;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+export const ProfileText = styled.span`
+    text-overflow: ellipsis;
+    font-size: 12px;
+    font-family: Poppins, sans-serif;
+    font-weight: 500;
+    text-align: center;
+    position: absolute;
+    left: 149px;
+    top: 319px;
+    position: absolute;
+    width: 144px;
+    height: 36px;
+    line-height: 18px;
+    color: var(--neutre-Black);
+`;
+
+export const BabyNewbie = styled.span`
+    color: black;
+    text-overflow: ellipsis;
+    font-size: 16px;
+    font-family: Poppins, sans-serif;
+    font-weight: 600;
+    text-align: left;
+    position: absolute;
+    left: 168px;
+    top: 361px;
+    width: 106px;
+    height: 24px;
+    line-height: 24px;
+    color: var(--neutre-Black);
+`;
--- Client/src/components/accountPage/top/badges/index.tsx
+++ Client/src/components/accountPage/top/badges/index.tsx
@@ -1,5 +1,115 @@
+import {
+    BadgeUnknown,
+    BadgeUnknown_0001,
+    BadgeUnknown_0002,
+    BadgeUnknown_0003,
+    BadgeUnknown_0004,
+    BadgeUnknown_0005,
+    BadgeUnknown_0006,
+    BadgeUnknown_0007,
+    BadgeUnknown_0008,
+    BadgeUnknown_0009,
+    BadgeUnknown_0010,
+    BadgeUnknown_0011,
+    Ellipse21,
+    Ellipse21_0001,
+    Frame58,
+    Group58,
+    BadgeText,
+    Group78,
+    Rectangle84,
+    Group56,
+    Ellipse20,
+    BabyNewbie,
+    ProfileText,
+} from "./BadgesSection.styled";
+
 const BadgesSection = () => {
-    return <div>Badges</div>;
+    return (
+        <>
+            <Frame58>
+                <Group78>
+                    <Rectangle84 />
+                </Group78>
+                <Group56>
+                    <Ellipse20
+                        src="src/assets/accountPage/baby_newbie_badge.png"
+                        alt="image of Ellipse20"
+                    />
+                </Group56>
+                <ProfileText>
+                    2023.05.15
+                    <br />
+                    Connect wallet to NICO!
+                </ProfileText>
+                <BabyNewbie>Baby Newbie</BabyNewbie>
+                <BadgeUnknown>
+                    <Group58>
+                        <Ellipse21
+                            src="src/assets/accountPage/coding_badge.png"
+                            alt="image of Ellipse21"
+                        />
+                    </Group58>
+                    <BadgeText>Coding King</BadgeText>
+                </BadgeUnknown>
+                <BadgeUnknown_0001>
+                    <Group58>
+                        <Ellipse21_0001 src="src/assets/accountPage/lock_icon.svg" />
+                    </Group58>
+                </BadgeUnknown_0001>
+                <BadgeUnknown_0002>
+                    <Group58>
+                        <Ellipse21_0001 src="src/assets/accountPage/lock_icon.svg" />
+                    </Group58>
+                </BadgeUnknown_0002>
+                <BadgeUnknown_0003>
+                    <Group58>
+                        <Ellipse21_0001 src="src/assets/accountPage/lock_icon.svg" />
+                    </Group58>
+                </BadgeUnknown_0003>
+                <BadgeUnknown_0004>
+                    <Group58>
+                        <Ellipse21_0001 src="src/assets/accountPage/lock_icon.svg" />
+                    </Group58>
+                </BadgeUnknown_0004>
+                <BadgeUnknown_0005>
+                    <Group58>
+                        <Ellipse21_0001 src="src/assets/accountPage/lock_icon.svg" />
+                    </Group58>
+                </BadgeUnknown_0005>
+                <BadgeUnknown_0006>
+                    <Group58>
+                        <Ellipse21_0001 src="src/assets/accountPage/lock_icon.svg" />
+                    </Group58>
+                </BadgeUnknown_0006>
+                <BadgeUnknown_0007>
+                    <Group58>
+                        <Ellipse21_0001 src="src/assets/accountPage/lock_icon.svg" />
+                    </Group58>
+                </BadgeUnknown_0007>
+                <BadgeUnknown_0008>
+                    <Group58>
+                        <Ellipse21_0001 src="src/assets/accountPage/lock_icon.svg" />
+                    </Group58>
+                </BadgeUnknown_0008>
+                <BadgeUnknown_0009>
+                    <Group58>
+                        <Ellipse21_0001 src="src/assets/accountPage/lock_icon.svg" />
+                    </Group58>
+                </BadgeUnknown_0009>
+                <BadgeUnknown_0010>
+                    <Group58>
+                        <Ellipse21_0001 src="src/assets/accountPage/lock_icon.svg" />
+                    </Group58>
+                </BadgeUnknown_0010>
+                <BadgeUnknown_0011>
+                    <Group58>
+                        <Ellipse21_0001 src="src/assets/accountPage/lock_icon.svg" />
+                    </Group58>
+                </BadgeUnknown_0011>
+            </Frame58>
+        </>
+    );
 };
 
 export default BadgesSection;
