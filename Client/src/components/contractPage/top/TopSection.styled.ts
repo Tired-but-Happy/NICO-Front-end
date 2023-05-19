72c721e52d4b95bedf3e7c7730c0b0a42b654cea
supersett <nolang3748@naver.com>
2023-05-20 02:34:34 +0900

    style: contract page

---
+++ Client/src/components/contractPage/top/TopSection.styled.ts
@@ -0,0 +1,888 @@
+import { styled } from "styled-components";
+
+export const Contract = styled.span`
+    color: rgb(60, 60, 64);
+    text-overflow: ellipsis;
+    font-size: 16px;
+    font-family: Poppins, sans-serif;
+    font-weight: 600;
+    text-align: left;
+    position: absolute;
+    left: 155px;
+    top: 172px;
+`;
+
+export const NaN_0001 = styled.div`
+    width: 1614px;
+    height: 360px;
+    position: absolute;
+    left: 155px;
+    top: 236px;
+`;
+
+export const Group79 = styled.div`
+    width: 1614px;
+    height: 360px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const Rectangle42 = styled.div`
+    width: 1614px;
+    height: 360px;
+    background-color: rgb(254, 252, 201);
+    border-radius: 40px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const Group88 = styled.div`
+    width: 457px;
+    height: 322px;
+    position: absolute;
+    left: 628px;
+    top: 16px;
+`;
+
+export const Frame59 = styled.div`
+    width: 457px;
+    height: 322px;
+    position: absolute;
+    left: 0px;
+    top: 5px;
+`;
+
+export const Rectangle95 = styled.div`
+    width: 457px;
+    height: 272px;
+    background-color: white;
+    border-radius: 40px;
+    position: absolute;
+    left: 0px;
+    top: 50px;
+`;
+
+export const MonthlyPriceChart = styled.span`
+    color: black;
+    text-overflow: ellipsis;
+    font-size: 28px;
+    font-family: Poppins, sans-serif;
+    font-weight: 600;
+    text-align: left;
+    position: absolute;
+    left: 20px;
+    top: 0px;
+`;
+
+export const Frame60 = styled.div`
+    width: 457px;
+    height: 322px;
+    position: absolute;
+    left: 1117px;
+    top: 21px;
+`;
+
+export const MonthlyTxnActivity = styled.span`
+    color: black;
+    text-overflow: ellipsis;
+    font-size: 28px;
+    font-family: Poppins, sans-serif;
+    font-weight: 600;
+    text-align: left;
+    position: absolute;
+    left: 20px;
+    top: 0px;
+`;
+
+export const Chart4 = styled.div`
+    width: 344px;
+    height: 276px;
+    position: absolute;
+    left: 665px;
+    top: 66px;
+`;
+
+export const Container = styled.div`
+    width: 344px;
+    height: 276px;
+    box-shadow: 0px 1px 4px rgba(13, 10, 44, 0.08);
+    background-color: white;
+    border-radius: 13px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const Weeks = styled.div`
+    width: 285px;
+    height: 11px;
+    position: absolute;
+    left: 45px;
+    top: 244px;
+`;
+
+export const Week1 = styled.span`
+    color: rgb(97, 94, 131);
+    text-overflow: ellipsis;
+    font-size: 9px;
+    font-family: Inter, sans-serif;
+    font-weight: 600;
+    line-height: 11px;
+    text-align: center;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const Week2 = styled.span`
+    color: rgb(97, 94, 131);
+    text-overflow: ellipsis;
+    font-size: 9px;
+    font-family: Inter, sans-serif;
+    font-weight: 600;
+    line-height: 11px;
+    text-align: center;
+    position: absolute;
+    left: 75px;
+    top: 0px;
+`;
+
+export const Week3 = styled.span`
+    color: rgb(97, 94, 131);
+    text-overflow: ellipsis;
+    font-size: 9px;
+    font-family: Inter, sans-serif;
+    font-weight: 600;
+    line-height: 11px;
+    text-align: center;
+    position: absolute;
+    left: 167px;
+    top: 0px;
+`;
+
+export const Week4 = styled.span`
+    color: rgb(97, 94, 131);
+    text-overflow: ellipsis;
+    font-size: 9px;
+    font-family: Inter, sans-serif;
+    font-weight: 600;
+    line-height: 11px;
+    text-align: center;
+    position: absolute;
+    left: 243px;
+    top: 0px;
+`;
+
+export const Amounts = styled.div`
+    width: 16px;
+    height: 130px;
+    position: absolute;
+    left: 23px;
+    top: 93px;
+`;
+
+export const Amounts_0 = styled.span`
+    color: rgb(97, 94, 131);
+    text-overflow: ellipsis;
+    font-size: 9px;
+    font-family: Inter, sans-serif;
+    font-weight: 600;
+    line-height: 11px;
+    text-align: right;
+    position: absolute;
+    left: 9px;
+    top: 119px;
+`;
+
+export const Amounts_1K = styled.span`
+    color: rgb(97, 94, 131);
+    text-overflow: ellipsis;
+    font-size: 9px;
+    font-family: Inter, sans-serif;
+    font-weight: 600;
+    line-height: 11px;
+    text-align: right;
+    position: absolute;
+    left: 5px;
+    top: 80px;
+`;
+
+export const Amounts_5K = styled.span`
+    color: rgb(97, 94, 131);
+    text-overflow: ellipsis;
+    font-size: 9px;
+    font-family: Inter, sans-serif;
+    font-weight: 600;
+    line-height: 11px;
+    text-align: right;
+    position: absolute;
+    left: 4px;
+    top: 40px;
+`;
+
+export const Amounts_10K = styled.span`
+    color: rgb(97, 94, 131);
+    text-overflow: ellipsis;
+    font-size: 9px;
+    font-family: Inter, sans-serif;
+    font-weight: 600;
+    line-height: 11px;
+    text-align: right;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const ChartLines = styled.div`
+    width: 277px;
+    height: 137px;
+    position: absolute;
+    left: 45px;
+    top: 98px;
+`;
+
+export const VerticalLines = styled.div`
+    width: 276px;
+    height: 137px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const Divider = styled.div`
+    width: 137px;
+    height: 0px;
+    border-top: solid 1px rgb(229, 229, 239);
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    transform: rotate(-270deg);
+    transform-origin: top left;
+`;
+
+export const Divider_0001 = styled.div`
+    width: 137px;
+    height: 0px;
+    border-top: solid 1px rgb(229, 229, 239);
+    position: absolute;
+    left: 92px;
+    top: 0px;
+    transform: rotate(-270deg);
+    transform-origin: top left;
+`;
+
+export const Divider_0002 = styled.div`
+    width: 137px;
+    height: 0px;
+    border-top: solid 1px rgb(229, 229, 239);
+    position: absolute;
+    left: 184px;
+    top: 0px;
+    transform: rotate(-270deg);
+    transform-origin: top left;
+`;
+
+export const Divider_0003 = styled.div`
+    width: 137px;
+    height: 0px;
+    border-top: solid 1px rgb(229, 229, 239);
+    position: absolute;
+    left: 276px;
+    top: 0px;
+    transform: rotate(-270deg);
+    transform-origin: top left;
+`;
+
+export const HozitontalLines = styled.div`
+    width: 277px;
+    height: 137px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const Baseline = styled.div`
+    width: 277px;
+    height: 0px;
+    border-top: solid 1px rgb(229, 229, 239);
+    position: absolute;
+    left: 277px;
+    top: 137px;
+    transform: rotate(-180deg);
+    transform-origin: top left;
+`;
+
+export const Line3 = styled.div`
+    width: 277px;
+    height: 0px;
+    border-top: dashed 1px rgb(229, 229, 239);
+    position: absolute;
+    left: 0px;
+    top: 79px;
+`;
+
+export const Line2 = styled.div`
+    width: 277px;
+    height: 0px;
+    border-top: dashed 1px rgb(229, 229, 239);
+    position: absolute;
+    left: 0px;
+    top: 40px;
+`;
+
+export const Line1 = styled.div`
+    width: 277px;
+    height: 0px;
+    border-top: dashed 1px rgb(229, 229, 239);
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const Lines = styled.div`
+    width: 275px;
+    height: 102px;
+    position: absolute;
+    left: 46px;
+    top: 108px;
+`;
+
+export const Line2_0001 = styled.img`
+    width: 273px;
+    height: 93px;
+    object-fit: cover;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const Line1_0001 = styled.img`
+    width: 275px;
+    height: 92px;
+    object-fit: cover;
+    position: absolute;
+    left: 0px;
+    top: 10px;
+`;
+
+export const Divider_0004 = styled.div`
+    width: 299px;
+    height: 0px;
+    border-top: solid 1px rgb(229, 229, 239);
+    position: absolute;
+    left: 23px;
+    top: 72px;
+`;
+
+export const Identifiers = styled.div`
+    width: 64px;
+    height: 28px;
+    position: absolute;
+    left: 257px;
+    top: 25px;
+`;
+
+export const Label1 = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    flex-direction: row;
+    align-items: center;
+    gap: 5px;
+    border-radius: 7px;
+    box-sizing: border-box;
+    position: absolute;
+    top: 0px;
+    right: 1px;
+    width: 75px;
+    height: 11px;
+`;
+
+export const Identifier = styled.div`
+    width: 7px;
+    height: 7px;
+    background-color: rgb(150, 45, 255);
+    border-radius: 7px;
+`;
+
+export const Name = styled.span`
+    color: rgb(97, 94, 131);
+    text-overflow: ellipsis;
+    font-size: 9px;
+    font-family: Inter, sans-serif;
+    font-weight: 600;
+    line-height: 11px;
+    text-align: left;
+`;
+
+export const Label2 = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    flex-direction: row;
+    align-items: center;
+    gap: 6px;
+    border-radius: 7px;
+    box-sizing: border-box;
+    position: absolute;
+    top: 17px;
+    right: 0px;
+    width: 75px;
+    height: 11px;
+`;
+
+export const Identifier_0001 = styled.div`
+    width: 7px;
+    height: 7px;
+    background-color: rgb(252, 181, 195);
+    border-radius: 5px;
+`;
+
+export const CardInfo = styled.div`
+    width: 135px;
+    height: 35px;
+    position: absolute;
+    left: 23px;
+    top: 22px;
+`;
+
+export const ComparionInfo = styled.span`
+    color: rgb(146, 145, 165);
+    text-overflow: ellipsis;
+    font-size: 9px;
+    font-family: Inter, sans-serif;
+    font-weight: 500;
+    line-height: 11px;
+    text-align: left;
+    position: absolute;
+    left: 47px;
+    top: 22px;
+`;
+
+export const CardTitle = styled.span`
+    color: rgb(30, 27, 57);
+    text-overflow: ellipsis;
+    font-size: 15px;
+    font-family: Inter, sans-serif;
+    font-weight: 600;
+    line-height: 19px;
+    text-align: left;
+    position: absolute;
+    left: 0px;
+    top: 16px;
+`;
+
+export const TypeCard = styled.span`
+    color: rgb(146, 145, 165);
+    text-overflow: ellipsis;
+    font-size: 12px;
+    font-family: Inter, sans-serif;
+    font-weight: 500;
+    line-height: 13px;
+    text-align: left;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const Chart3 = styled.div`
+    width: 344px;
+    height: 276px;
+    position: absolute;
+    left: 1161px;
+    top: 66px;
+`;
+
+export const Group84 = styled.div`
+    width: 428px;
+    height: 88px;
+    position: absolute;
+    left: 48px;
+    top: 152px;
+`;
+
+export const Rectangle95_0001 = styled.div`
+    width: 428px;
+    height: 88px;
+    background-color: white;
+    border-radius: 12px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const Group80 = styled.div`
+    width: 428px;
+    height: 36px;
+    position: absolute;
+    left: 0px;
+    top: 8px;
+`;
+
+export const Group82 = styled.div`
+    width: 428px;
+    height: 36px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const Component20 = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+    background-color: white;
+    box-sizing: border-box;
+    padding-right: 16px;
+    position: absolute;
+    left: calc((calc((50% + -161px)) - 53px));
+    top: calc((calc((50% + 0px)) - 18px));
+    width: 107px;
+    height: 36px;
+`;
+
+export const Section = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: row;
+    align-items: center;
+    flex: none;
+    gap: 6px;
+    height: 36px;
+    box-sizing: border-box;
+    padding-left: 16px;
+    width: 126.66px;
+`;
+
+export const MarketCap = styled.span`
+    color: rgb(182, 181, 183);
+    text-overflow: ellipsis;
+    font-size: 14px;
+    font-family: Poppins, sans-serif;
+    font-weight: 500;
+    text-align: left;
+`;
+
+export const Component18 = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 10px;
+    background-color: white;
+    box-sizing: border-box;
+    padding-right: 24px;
+    position: absolute;
+    left: 227px;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+`;
+
+export const Section_0001 = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    flex-direction: row;
+    align-items: center;
+    flex: 1;
+    gap: 24px;
+    height: 36px;
+    box-sizing: border-box;
+`;
+
+export const Section_0001Text = styled.span`
+    color: black;
+    text-overflow: ellipsis;
+    font-size: 16px;
+    font-family: Poppins, sans-serif;
+    font-weight: 600;
+    text-align: right;
+`;
+
+export const Group83 = styled.div`
+    width: 428px;
+    height: 36px;
+    position: absolute;
+    left: 0px;
+    top: 44px;
+`;
+
+export const MarketCapRank = styled.span`
+    color: rgb(182, 181, 183);
+    text-overflow: ellipsis;
+    font-size: 14px;
+    font-family: Poppins, sans-serif;
+    font-weight: 500;
+    text-align: left;
+`;
+
+export const Group81 = styled.div`
+    width: 200px;
+    height: 36px;
+    position: absolute;
+    left: 228px;
+    top: 0px;
+`;
+
+export const Component18_0001 = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 10px;
+    background-color: white;
+    box-sizing: border-box;
+    padding-right: 24px;
+    position: absolute;
+    left: 0px;
+    top: calc((calc((50% + 0px)) - 18px));
+    right: 0px;
+    height: 36px;
+`;
+
+export const Section_0003 = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: row;
+    align-items: center;
+    flex: none;
+    gap: 24px;
+    box-sizing: border-box;
+`;
+
+export const Section_0005_234 = styled.span`
+    color: black;
+    text-overflow: ellipsis;
+    font-size: 16px;
+    font-family: Poppins, sans-serif;
+    font-weight: 600;
+    text-align: left;
+`;
+
+export const Group87 = styled.div`
+    width: 428px;
+    height: 88px;
+    position: absolute;
+    left: 48px;
+    top: 252px;
+`;
+
+export const Rectangle96 = styled.div`
+    width: 428px;
+    height: 88px;
+    background-color: white;
+    border-radius: 12px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const Section_0004 = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: row;
+    align-items: center;
+    gap: 6px;
+    box-sizing: border-box;
+    padding-left: 16px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    width: 147px;
+    height: 36px;
+`;
+
+export const PriceAthAtl = styled.span`
+    color: rgb(182, 181, 183);
+    text-overflow: ellipsis;
+    font-size: 14px;
+    font-family: Poppins, sans-serif;
+    font-weight: 500;
+    text-align: left;
+`;
+
+export const RiInformationLine = styled.div`
+    width: 24px;
+    height: 24px;
+    overflow: hidden;
+    position: relative;
+`;
+
+export const Vector_0027 = styled.svg`
+    width: 20px;
+    height: 20px;
+    position: absolute;
+    left: 2px;
+    top: 2px;
+    right: 2px;
+    bottom: 2px;
+`;
+
+export const Component17 = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 10px;
+    background-color: white;
+    box-sizing: border-box;
+    padding-right: 24px;
+    position: absolute;
+    left: 228px;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+`;
+
+export const Section_0005 = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: row;
+    align-items: center;
+    flex: 1;
+    gap: 24px;
+    height: 36px;
+    box-sizing: border-box;
+`;
+
+export const Section_0006 = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: row;
+    align-items: center;
+    gap: 6px;
+    box-sizing: border-box;
+    padding-left: 16px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    width: 70px;
+    height: 36px;
+`;
+
+export const Tvl = styled.span`
+    color: rgb(182, 181, 183);
+    text-overflow: ellipsis;
+    font-size: 14px;
+    font-family: Poppins, sans-serif;
+    font-weight: 500;
+    text-align: left;
+`;
+
+export const Component18_0002 = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    align-items: flex-end;
+    gap: 10px;
+    background-color: white;
+    box-sizing: border-box;
+    padding-right: 24px;
+    position: absolute;
+    left: 228px;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+`;
+
+export const NaN_0002 = styled.span`
+    color: black;
+    text-overflow: ellipsis;
+    font-size: 16px;
+    font-family: Poppins, sans-serif;
+    font-weight: 600;
+    text-align: left;
+`;
+
+export const Group108 = styled.div`
+    width: 428px;
+    height: 116px;
+    position: absolute;
+    left: 48px;
+    top: 24px;
+`;
+
+export const Rectangle94 = styled.div`
+    width: 428px;
+    height: 116px;
+    background-color: white;
+    border-radius: 8px;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const Group107 = styled.div`
+    width: 428px;
+    height: 83px;
+    position: absolute;
+    left: -0px;
+    top: 25px;
+`;
+
+export const Group84_0001 = styled.div`
+    width: 428px;
+    height: 36px;
+    position: absolute;
+    left: -0px;
+    top: 47px;
+`;
+
+export const Component20_0001 = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+    background-color: white;
+    box-sizing: border-box;
+    padding-right: 16px;
+    position: absolute;
+    left: calc((calc((50% + -161px)) - 53px));
+    top: calc((calc((50% + 0px)) - 18px));
+    width: 107px;
+    height: 36px;
+`;
+
+export const Section__24HVolume = styled.span`
+    color: rgb(182, 181, 183);
+    text-overflow: ellipsis;
+    font-size: 14px;
+    font-family: Poppins, sans-serif;
+    font-weight: 500;
+    text-align: left;
+`;
+
+export const Group106 = styled.div`
+    width: 388px;
+    height: 42px;
+    position: absolute;
+    left: 16px;
+    top: 0px;
+`;
+
+export const Sweat = styled.span`
+    color: black;
+    text-overflow: ellipsis;
+    font-size: 28px;
+    font-family: Poppins, sans-serif;
+    font-weight: 600;
+    text-align: right;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+`;
+
+export const SweatText = styled.span`
+    color: black;
+    text-overflow: ellipsis;
+    font-size: 28px;
+    font-family: Poppins, sans-serif;
+    font-weight: 600;
+    text-align: right;
+    position: absolute;
+    left: 247px;
+    top: 0px;
+`;
