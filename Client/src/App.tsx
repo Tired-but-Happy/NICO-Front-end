5476c77dca7752a9bed77df0192a89aeef79de24
Junhyuk Park <junhyeg08@gmail.com>
2023-05-20 23:31:55 +0900

    fix: near provider

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,6 @@
 import { GlobalStyles } from "src/styles/GlobalStyles.styled";
 import Router from "src/Router";
-<<<<<<< HEAD
-=======
-import getAccountDetail from "src/near/utils";
 import { NearProvider } from "./NearContext";
->>>>>>> origin
 
 const App = () => {
     return (
