dd3579d7ea9c61a084e699643815bf8a118d8d01
Junhyuk Park <junhyeg08@gmail.com>
2023-05-21 03:25:03 +0900

    feat: background

--- Client/src/pages/LandingPage.tsx
+++ Client/src/pages/LandingPage.tsx
@@ -1,10 +1,14 @@
 import LatestInfoSection from "src/components/landingPage/latestInfoSection";
 import SearchTextField from "src/components/landingPage/searchTextField";
 import TopicSection from "src/components/landingPage/topicSection";
+import { BackGroundBox, Rectangle2 } from "src/components/layout/BackGround.styled";
 
 const LandingPage = () => {
     return (
         <>
+            <BackGroundBox>
+                <Rectangle2 />
+            </BackGroundBox>
             <SearchTextField />
             <TopicSection />
             <LatestInfoSection />
